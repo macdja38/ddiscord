85a63809f8a01ab5b22413b3ab284f74601505dd
Patrick Gingras <775.pg.12@gmail.com>
2019-02-10 08:10:53 -0500

    added some awaits

--- packages/event-bot/lib/event-bot.js
+++ packages/event-bot/lib/event-bot.js
@@ -10,7 +10,7 @@ async function eventBot({ token, topicName, ...options }) {
     state.events += 1;
   });
 
-  client.on('MESSAGE_CREATE', ({ d: { content, channel_id: channelId } }) => {
+  client.on('MESSAGE_CREATE', async ({ d: { content, channel_id: channelId } }) => {
     if (!content.startsWith('?event-bot')) {
       return;
     }
@@ -18,11 +18,11 @@ async function eventBot({ token, topicName, ...options }) {
 
     switch (command) {
       case 'total':
-        client.channel.createMessage(channelId, code(`Total events to date: ${state.events}`));
+        await client.channel.createMessage(channelId, code(`Total events to date: ${state.events}`));
         break;
       case 'per second': {
         const average = state.events / (Date.now() - state.start) * 1000;
-        client.channel.createMessage(channelId, code(`Average events per second: ${average}`));
+        await client.channel.createMessage(channelId, code(`Average events per second: ${average}`));
         break;
       }
       default:

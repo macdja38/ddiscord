7c7c7e651127055267fb1e396a943c0a3c12f2aa
Patrick Gingras <775.pg.12@gmail.com>
2019-02-10 08:06:57 -0500

    made event-bot track number of events

--- packages/event-bot/lib/event-bot.js
+++ packages/event-bot/lib/event-bot.js
@@ -1,13 +1,33 @@
 const Client = require('discord-client');
 
+const code = (content, type = '') => `\`\`\`${type}\n${content}\`\`\``;
+
+const state = { events: 0, start: Date.now() };
 async function eventBot({ token, topicName, ...options }) {
   const client = new Client(token);
 
-  client.on('*', (m) => {
-    client.channel.createMessage(
-      '233647266957623297',
-      `\`\`\`json\n${JSON.stringify(m, null, 2).slice(0, 1500)}\`\`\``,
-    );
+  client.on('*', () => {
+    state.events += 1;
+  });
+
+  client.on('MESSAGE_CREATE', ({ d: { content, channel_id: channelId } }) => {
+    if (!content.startsWith('?event-bot')) {
+      return;
+    }
+    const command = content.substring(0, '?event-bot '.length);
+
+    switch (command) {
+      case 'total':
+        client.channel.createMessage(channelId, code(`Total events to date: ${state.events}`));
+        break;
+      case 'per second': {
+        const average = state.events / (Date.now() - state.start) * 1000;
+        client.channel.createMessage(channelId, code(`Average events per second: ${average}`));
+        break;
+      }
+      default:
+        break;
+    }
   });
 
   await client.connect(options);
